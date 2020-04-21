87bee3812a8eddd73571c8206f84253d9d733ef3
Pablo Garrido <pablogs9@gmail.com>
2020-04-21 11:44:18 +0200

    Update topic names

--- open_manipulator_x_tof/src/open_manipulator_x_tof.cpp
+++ open_manipulator_x_tof/src/open_manipulator_x_tof.cpp
@@ -27,9 +27,9 @@ OpenManipulatorXToFDemo::OpenManipulatorXToFDemo()
   qos_besteffort.best_effort();
 
   joint_states_sub_ = this->create_subscription<sensor_msgs::msg::JointState>(
-    "open_manipulator_x/joint_states", qos, std::bind(&OpenManipulatorXToFDemo::joint_states_callback, this, _1));
+    "joint_states", qos, std::bind(&OpenManipulatorXToFDemo::joint_states_callback, this, _1));
   kinematics_pose_sub_ = this->create_subscription<open_manipulator_msgs::msg::KinematicsPose>(
-    "open_manipulator_x/kinematics_pose", qos, std::bind(&OpenManipulatorXToFDemo::kinematics_pose_callback, this, _1));
+    "kinematics_pose", qos, std::bind(&OpenManipulatorXToFDemo::kinematics_pose_callback, this, _1));
   tof_sub_ = this->create_subscription<std_msgs::msg::Float32>(
     "sensors/tof", qos_besteffort, std::bind(&OpenManipulatorXToFDemo::tof_sensor_callback, this, _1));
   
@@ -69,13 +69,13 @@ OpenManipulatorXToFDemo::OpenManipulatorXToFDemo()
   ** Initialise Clients
   ********************************************************************************/
   goal_joint_space_path_client_ = this->create_client<open_manipulator_msgs::srv::SetJointPosition>(
-    "open_manipulator_x/goal_joint_space_path");
+    "goal_joint_space_path");
   goal_tool_control_client_ = this->create_client<open_manipulator_msgs::srv::SetJointPosition>(
-    "open_manipulator_x/goal_tool_control");
+    "goal_tool_control");
   goal_task_space_path_from_present_position_only_client_ = this->create_client<open_manipulator_msgs::srv::SetKinematicsPose>(
-    "open_manipulator_x/goal_task_space_path_from_present_position_only");
+    "goal_task_space_path_from_present_position_only");
   goal_joint_space_path_from_present_client_ = this->create_client<open_manipulator_msgs::srv::SetJointPosition>(
-    "open_manipulator_x/goal_joint_space_path_from_present");
+    "goal_joint_space_path_from_present");
 
   /********************************************************************************
   ** Display in terminal
